281e1b5dec559801682e2d9baf610bcdf9072550
Edwuardito <a20edwin02@gmail.com>
2024-04-22 23:18:19 -0300

    portfolio v1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,24 @@
-import logo from "../assets/kevinRushLogo.png"
 import { FaLinkedin } from "react-icons/fa"
 import { FaGithub } from "react-icons/fa"
 import { FaSquareXTwitter } from "react-icons/fa6"
 import { FaInstagram } from "react-icons/fa"
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
+    <nav className="flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2  w-10" src={logo} alt="logo" />
+            <h1 className="mx-2 text-4xl w-10">
+              Er  
+            </h1>
         </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2l">
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+          <a href="https://www.linkedin.com/in/edwin-rodriguez-garcia-7b717a16b">
             <FaLinkedin/>
+          </a>
+          <a href="https://github.com/Edwuardito">
             <FaGithub/>
-            <FaInstagram />
-            <FaSquareXTwitter/>
+          </a>
+            {/* <FaInstagram />
+            <FaSquareXTwitter/> */}
         </div>
     </nav>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,31 @@
 import { PROJECTS } from "../constants"
+import { motion } from "framer-motion"
 
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-        <h1 className="my-20 text-center text-4xl">Projects</h1>
+        <motion.h1 className="my-20 text-center text-4xl"
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 1, y: -100 }}
+            transition={{ duration: 0.5 }}
+        >
+            Projects
+        </motion.h1>
         <div>
             {PROJECTS.map((project, index) => (
                 <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-                    <div className="w-full  lg:w-1/4">
-                        <img src={project.image} width={150} height={150} alt={project.title} className="mb-6 rounded"/>
-                    </div>
-                    <div className="w-full max-w-xl lg:w-3/4">
+                    <motion.div className="w-full  lg:w-1/4"
+                        whileInView={{ opacity: 1, x: 0 }}
+                        initial={{ opacity: 1, x: -100 }}
+                        transition={{ duration: 1 }}
+                    >
+                        <img src={project.image} width={150} height={150} alt={project.title} className="mb-6 rounded w-40 h-40 object-cover object-center"/>
+                    </motion.div>
+                    <motion.div className="w-full max-w-xl lg:w-3/4"
+                        whileInView={{ opacity: 1, x: 0 }}
+                        initial={{ opacity: 1, x: 100 }}
+                        transition={{ duration: 1 }}
+                    >
                         <h6 className="mb-2 font-semibold">{project.title}</h6>
                         <p className="mb-4 text-neutral-400">{project.description}</p>
                         {
@@ -18,7 +33,10 @@ const Projects = () => {
                                 <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900">{tech}</span>
                             ))
                         }
-                    </div>
+                        <a href="https://frontpagebyedu.netlify.app">
+                            <button className="ml-10  px-2 bg-purple-900 rounded text-white">Visitar</button>
+                        </a>
+                    </motion.div>
                 </div>
             ))}
         </div>
