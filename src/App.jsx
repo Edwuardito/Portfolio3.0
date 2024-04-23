ea172e24754e8b5fa87e51fdaeecab7a23eb614a
Edwuardito <a20edwin02@gmail.com>
2024-04-23 12:12:36 -0300

    lenguage changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from "./components/Contact";
 import { useState } from "react";
 
 export default function App() {
-  const [ lenguage, setLenguage ] = useState('es') 
+  const [ lenguage, setLenguage ] = useState('en') 
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
